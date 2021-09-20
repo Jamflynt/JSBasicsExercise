153311e3833ebe88e1e379a2248fffe5f4eee035
Jamie Flynt <jamflynt@gmail.com>
2021-09-20 16:40:30 -0400

    Finished exercise. Finished bonus.

---
+++ script.js
@@ -0,0 +1,51 @@
+// 1
+console.log("8");
+
+// 2
+// Numbers, Strings, Booleans, Null, Undefined
+
+// 3
+// + - / * ** %
+
+// 4
+console.log(6%5);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6
+console.log("Hawk"+"eye");
+
+// 7
+console.log("Jamie".length+"Flynt".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// Bonus
+// 1
+console.log("\\");
+
+// 2
+console.log(5**4-9);
+
+// 3
+console.log("I".length-"Am".length+"Iron".length-"Man".length);
+console.log("I".length+"Am".length%"Iron".length-"Man".length); //extra bonus
+
+// 4
+console.log("CAP"[1]); 
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m=25; 
+var c=4;
+var u=m+c
+console.log(u);
